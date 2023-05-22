daace8f602bee193edae39b259987d2d6f7648d8
Farhan Younas <farhanyounas5204@gmail.com>
2023-05-22 23:54:54 +0500

    Add files via upload

---
+++ if clause.py
@@ -0,0 +1,30 @@
+# age=int(input("Enter your age\n"))
+# if(age>20):
+#     print("you are 20 years old")
+
+# else:
+#     print("you are under 20 so you are not ellgible")
+
+
+#### write a program to print yes when the age is enteree by the user is greater then or equal to 18
+
+
+# age=int(input("Enter your age\n"))
+# if(age>=18):
+#     print("yes")
+# else:
+#     print("no")
+
+
+
+age=int(input("Enter your age"))
+if(age>30):
+    print("your are elligible")
+elif(age>25):
+    print("you are exeptional case")
+
+elif(age>=20):
+    print("you are selected")
+
+else:
+    print("sorry")
